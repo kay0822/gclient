2d3178ad2aabaee64ae1fd6f5acd96d097f23d5a
root <root@(none).(none)>
2013-07-10 22:22:49 +0000

    test submenu

--- client/gclient.c
+++ client/gclient.c
@@ -5,6 +5,16 @@
 #include <core/gtk.h>
 
 //#include <cairo.h>
+gboolean show(GtkWidget* widget, GdkEventKey * event, gpointer data){
+	DEBUG_MSG("start clicked\n");
+	return TRUE;
+}
+
+gboolean key_value(GtkWidget* widget, GdkEventKey * event, gpointer data){
+	guint keyvalue = event->keyval;
+	DEBUG_MSG("%d \n", keyvalue);
+	return TRUE;
+}
 
 int main(int argc, char* argv[]){
 	/* Just test widget */
@@ -26,6 +36,9 @@ int main(int argc, char* argv[]){
 	/* Menu */
 	GtkWidget *fixed, *vbox, *menubar, *filemenu, *file, *new;
 	GtkWidget *menustart, *menuitem;
+	GtkWidget *menustart2;
+	GtkWidget *browser, *protocol, *quit, *ica, *rdp, *menuitem2;
+	GtkAccelGroup *accel_group = NULL;
 
 #if 1
 	fixed = gtk_fixed_new();
@@ -36,18 +49,48 @@ int main(int argc, char* argv[]){
 	gtk_fixed_put(GTK_FIXED(fixed), menubar, 0, height - _HEIGHT);
 	gtk_widget_set_size_request(menubar, _WIDTH, _HEIGHT); 
 
-	menustart = gtk_menu_item_new_with_label("start");
+	vbox = gtk_vbox_new(FALSE, 1);
+	gtk_container_border_width(GTK_CONTAINER(vbox), 1);
+	//gtk_container_add(GTK_CONTAINER(window), protocol	
+	accel_group = gtk_accel_group_new();
+	gtk_window_add_accel_group(GTK_WINDOW(window), accel_group);
+
+	//menustart = gtk_menu_item_new_with_label("start");
+	menustart = gtk_menu_item_new_with_mnemonic("_start");
 	gtk_menu_shell_append(GTK_MENU_SHELL(menubar), menustart);
+	menustart2 = gtk_menu_item_new_with_label("start2");
+	gtk_menu_shell_append(GTK_MENU_SHELL(menubar), menustart2);
+
+/*
+	GtkWidget *statusbar;
+	statusbar = gtk_statusbar_new();
+	gtk_statusbar_set_has_resize_grip(GTK_STATUSBAR(statusbar), FALSE);
+	gtk_fixed_put(GTK_FIXED(fixed), statusbar, 0, height - _HEIGHT - 30);
+	gtk_widget_set_size_request(statusbar, width, 30);
+DEBUG_MSG("gtk_statusbar_get_context_id = %d\n", gtk_statusbar_get_context_id(GTK_STATUSBAR(statusbar), "test"));
+	gtk_statusbar_push(GTK_STATUSBAR(statusbar), 10, "test");
+	//gtk_statusbar_push(GTK_STATUSBAR(statusbar), gtk_statusbar_get_context_id(GTK_STATUSBAR(statusbar), "test"), "test");
+*/
+
+/*
+	GtkWidget* entry = gtk_entry_new();
+	gtk_fixed_put(GTK_FIXED(fixed), entry, 0, 300);
+	g_signal_connect(entry, "key-press-event", G_CALLBACK(key_value), NULL);
+*/	
 
 	menuitem = gtk_menu_new();
 	gtk_menu_item_set_submenu(GTK_MENU_ITEM(menustart), menuitem);
 
-	GtkWidget *browser, *protocol, *ica, *rdp, *menuitem2;
-	browser = gtk_menu_item_new_with_label("browser");
-	protocol = gtk_menu_item_new_with_label("protocol");
+	//browser = gtk_menu_item_new_with_label("browser");
+	browser = gtk_image_menu_item_new_with_mnemonic("_browser");
+	//protocol = gtk_menu_item_new_with_label("protocol");
+	protocol = gtk_menu_item_new_with_mnemonic("_protocol");
+	quit = gtk_menu_item_new_with_label("quit");
 	gtk_menu_shell_append(GTK_MENU_SHELL(menuitem), browser);
 	gtk_menu_shell_append(GTK_MENU_SHELL(menuitem), protocol);
-
+	gtk_menu_shell_append(GTK_MENU_SHELL(menuitem), quit);
+	//gtk_widget_add_accelerator(protocol, "activate", accel_group, GDK_q, GDK_CONTROL_MASK, GTK_ACCEL_VISIBLE);
+	gtk_widget_add_accelerator(menustart, "activate", accel_group, GDK_q, GDK_CONTROL_MASK, GTK_ACCEL_VISIBLE);
 
 	menuitem2 = gtk_menu_new();
 	gtk_menu_item_set_submenu(GTK_MENU_ITEM(protocol), menuitem2);
@@ -55,6 +98,9 @@ int main(int argc, char* argv[]){
 	rdp = gtk_menu_item_new_with_label("RDP");
 	gtk_menu_shell_append(GTK_MENU_SHELL(gtk_menu_item_get_submenu(GTK_MENU_ITEM(protocol))), ica);
 	gtk_menu_shell_append(GTK_MENU_SHELL(gtk_menu_item_get_submenu(GTK_MENU_ITEM(protocol))), rdp);
+
+
+	g_signal_connect(G_OBJECT(menustart), "activate", G_CALLBACK(show), NULL);
 #endif
 	
 #if 0
